60fa089fb8d657ddd9dc5d342d1a5cf804cb44dc
unknown <Vali@.(none)>
2011-03-11 18:43:25 +0200

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :prepare_for_mobile
 
   ActiveScaffold.set_defaults do |config| 
     config.ignore_columns.add [:created_at, :updated_at, :lock_version]
@@ -10,4 +11,19 @@ class ApplicationController < ActionController::Base
     args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
     system "start rake #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log"
   end  
+  
+private
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+  
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
 end
