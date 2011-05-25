0340bf6da896c180ca87fc8ef5a8345ef21a6d3a
unknown <Vali@.(none)>
2011-05-25 17:58:15 +0300

    ok

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'ECY3RkpRtk7sNIa9OtELQ', 'X9oIRrQ9GSfQxKre7MmOM8gpoEx8dDOfZkX5paAQ'
-  provider :facebook, '627711beba2da431d2266bfe40c41618', 'e042af88df1ec657121d3609e353740a', {:client_options => {:ssl => {:ca_file => 'cert/ca-bundle.crt'}}}
+  provider :twitter, 'yABWbIRA0s0s1Jc81B8Dg', 'IuboXnkpR7HFXm4S60WBYCxQyvr41KWE0DNYFQmGU5s'
+  provider :facebook, 'be2ad4bb809aa5768ee82da32dad9d26', '4d86565c2e66cfb66d8abded500cc4f8', {:client_options => {:ssl => {:ca_file => 'cert/ca-bundle.crt'}}}
 end
 
